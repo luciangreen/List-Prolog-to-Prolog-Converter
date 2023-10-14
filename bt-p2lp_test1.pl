c9783a6f9ac1562f89a62ea5ba37df6904edbd45
Lucian Green <luciangreen@lucianacademy.com>
2023-10-15 09:36:18 +1100

    lp2p pretty printer verification

--- bt-p2lp_test1.pl
+++ bt-p2lp_test1.pl
@@ -9,12 +9,22 @@
 % B - output: result
 
 bt-p2lp_test(BL,RL) :-
-findall(A,(p2lp_test(N,_I,O),((lp2p1(O,I1),p2lpconverter([string,I1],O2),O=O2)->(writeln([bt-p2lp_test,N,passed]),A=passed);(writeln([bt-p2lp_test,N,failed],A=failed)))),B),
+findall(A,(p2lp_test(N,_I,O),((lp2p1(O,I1),p2lpconverter([string,I1],O2),O=O2)->(writeln([bt-p2lp_test,N,passed]),A=passed);(writeln([bt-p2lp_test,N,failed]),A=failed))),B),
 length(B,BL),
 findall(_,member(passed,B),R),length(R,RL),!.
 
 bt-p2lp_test1(N,A) :-
-p2lp_test(N,_I,O),((lp2p1(O,I1),p2lpconverter([string,I1],O2),O=O2)->(writeln([bt-p2lp_test,N,passed]),A=passed);(writeln([bt-p2lp_test,N,failed],A=failed))),!.
+p2lp_test(N,_I,O),((lp2p1(O,I1),p2lpconverter([string,I1],O2),O=O2)->(writeln([bt-p2lp_test,N,passed]),A=passed);(writeln([bt-p2lp_test,N,failed]),A=failed)),!.
+
+
+
+bt-p2lp_pp_test(BL,RL) :-
+findall(A,(p2lp_test(N,_I,O),((pp_lp2p0(O,I1),p2lpconverter([string,I1],O2),O=O2)->(writeln([bt-p2lp_test,N,passed]),A=passed);(writeln([bt-p2lp_test,N,failed]),A=failed))),B),
+length(B,BL),
+findall(_,member(passed,B),R),length(R,RL),!.
+
+bt-p2lp_pp_test1(N,A) :-
+p2lp_test(N,_I,O),((pp_lp2p0(O,I1),p2lpconverter([string,I1],O2),O=O2)->(writeln([bt-p2lp_test,N,passed]),A=passed);(writeln([bt-p2lp_test,N,failed]),A=failed)),!.
 
 % to do
 % back-translation, on I not O
