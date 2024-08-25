3f8633c42345cf3e6b5ba767d6139b6a22c6b764
Lucian Green <luciangreen@lucianacademy.com>
2024-08-25 14:29:33 +1000

    Commented out include p2lpconverter.pl

--- lp2pconverter.pl
+++ lp2pconverter.pl
@@ -2,5 +2,5 @@
 
 :-include('../listprologinterpreter/listprolog.pl').
 :-include('lp2pconverter1.pl').
-:-include('../Prolog-to-List-Prolog/p2lpconverter.pl').
+%:-include('../Prolog-to-List-Prolog/p2lpconverter.pl').
 :-include('bt-p2lp_test1.pl').
